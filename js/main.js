408da909eb0b9560d87933be79a00da6bc10eaa4
EduardoNogueira <eduardocastroemp@gmail.com>
2022-08-26 18:02:39 -0300

    ajuste nos serviços

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* Dinâmica do botão mobile */
-buttonMenu.addEventListener('click', () =>{
+    buttonMenu.addEventListener('click', () =>{
     menu.classList.toggle("ph-x");
     menu.classList.toggle("icone-animation");
     nav.classList.toggle("menu-nav_on")
@@ -20,7 +20,7 @@ const dashright = new IntersectionObserver( entries => {
 }, {
  threshold: [0.25 , 0.5 , 0.75 , 1]
 })
-Array.from(document.querySelectorAll('.inicio-titulo')).forEach( element => {
+Array.from(document.querySelectorAll('.inicio-titulo, .clientes-imagem, .mapa-titulo')).forEach( element => {
 dashright.observe(element)
 })
 /* Animação para a direita - fim */
@@ -38,26 +38,72 @@ const dashleft = new IntersectionObserver( entries => {
 }, {
  threshold: [0.25 , 0.5 , 0.75 , 1]
 })
-Array.from(document.querySelectorAll('.inicio-imagem')).forEach( element => {
+Array.from(document.querySelectorAll('.inicio-imagem, .rede-imagem, .mapa-imagem, .clientes-titulo')).forEach( element => {
 dashleft.observe(element)
 })
 /* Animação para a esquerda - fim*/
 
 
-/* Animação para baixo */
-const dashdown = new IntersectionObserver( entries => {
+/* Animação para cima */
+const dashup = new IntersectionObserver( entries => {
 
   Array.from(entries).forEach(entry => {
-    if (entry.intersectionRatio >= 0.5) {
-    entry.target.classList.add('dash-down')
+    if (entry.intersectionRatio >= 1) {
+    entry.target.classList.add('dash-up')
     }
   })
  
 }, {
  threshold: [0.25 , 0.5 , 0.75 , 1]
 })
-Array.from(document.querySelectorAll('.')).forEach( element => {
-dashdown.observe(element)
+Array.from(document.querySelectorAll('.servicos-vantagem, .vantagem-divisor_mobile')).forEach( element => {
+dashup.observe(element)
 })
 /* Animação para baixo - fim */
+
+
+/* Animação contador */
+let displayed = false;
+const contador = new IntersectionObserver( entries => {
+  console.log(entries)
+    Array.from(entries).forEach(entry => {
+      if (entry.intersectionRatio >= 1) {
+        function action(){
+            if( displayed == false){
+
+              for(cont = 1; cont <= 4 ; cont++){
+                const valor = document.getElementById("valor" + cont);
+                let max = valor.innerHTML;
+
+                for (i = 0; i <= max; i++){
+                    setTimeout(function(nr){
+                        valor.innerHTML = nr;
+                    }, i * 1500 / max, i)
+                }
+              }
+              displayed = true;
+            }
+        }
+
+        action()
+      }
+    })
+}, {
+ threshold: [0.25 , 0.5 , 0.75 , 1]
+})
+Array.from(document.querySelectorAll('.card-titulo')).forEach( element => {
+contador.observe(element)
+})
+/* Animação contador fim */
+
+
+/* Scrollspy */
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.scrollspy');
+  var instances = M.ScrollSpy.init(elems, 200  );
+});
+/* Scrollspy fim */
+
+
+
 
